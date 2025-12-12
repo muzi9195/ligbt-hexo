0962eaa276bdc87ce2c5372df80af9af955de59d
limpid <74402876+muzi9195@users.noreply.github.com>
2025-12-12 22:07:51 +0800

    更新 build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+rm -rf build
+rm -rf public
+# 生成hexo静态文件
+npm install hexo-cli
+npm install
+./node_modules/hexo-cli/bin/hexo generate
+hexo generate
+cp -r public build
 npm install
 npm update
 hexo clean
